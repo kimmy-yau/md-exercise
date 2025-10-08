0cf7db5b6049ba13c6773d822a8254b0013a59e6
kimmy-yau <kimberley.yau@outlook.com>
2025-10-08 11:46:54 +0100

    Step 2 : Add discount logic and updated Shopping Cart unit tests

--- src/main/java/com/md_exercise/utils/ShoppingCartUtils.java
+++ src/main/java/com/md_exercise/utils/ShoppingCartUtils.java
@@ -9,20 +9,39 @@ public class ShoppingCartUtils {
 	private final static String APPLE = "Apple";
 	private final static String ORANGE = "Orange";
 	
+	/**
+	 * Calculates the total of the shopping cart
+	 * Applies any applicable special offers
+	 * @param cart
+	 * @return total
+	 */
 	public static String calculateTotal(ShoppingCart cart) {
 		List<String> items = cart.getItems();
 		
 		double total = 0;
 		
+		int appleCounter = 0;
+		int orangeCounter = 0;
+		
 		for(String item:items) {
 			switch(item) {
 			
 				case APPLE: 
-					total += 0.60; 
+					appleCounter++;
+					
+					//Exclude adding for every 2nd apple
+					if(appleCounter % 2 > 0) {
+						total += 0.60; 
+					}
 					break;
 				
 				case ORANGE:
-					total += 0.25;
+					orangeCounter++;
+					
+					//Exclude adding for every 3rd orange
+					if(orangeCounter % 3 > 0) {
+						total += 0.25;
+					}
 					break;
 			}
 		}
--- src/test/java/com/md_exercise/utils/ShoppingCartUtilsTest.java
+++ src/test/java/com/md_exercise/utils/ShoppingCartUtilsTest.java
@@ -60,7 +60,84 @@ class ShoppingCartUtilsTest {
 		cart.setItems(itemsToAdd);
 		String result = ShoppingCartUtils.calculateTotal(cart);
 		
-		assertEquals("£2.05", result);			
+		assertEquals("£1.45", result);			
+	}
+	
+	@Test 
+	void calculateTotal_AppleOffer(){
+		ShoppingCart cart = new ShoppingCart();
+		List<String> itemsToAdd = Arrays.asList(APPLE,APPLE);
+		
+		cart.setItems(itemsToAdd);
+		String result = ShoppingCartUtils.calculateTotal(cart);
+		
+		assertEquals("£0.60", result);			
+	}
+	
+	@Test 
+	void calculateTotal_ThreeAppleOffer(){
+		ShoppingCart cart = new ShoppingCart();
+		List<String> itemsToAdd = Arrays.asList(APPLE,APPLE,APPLE);
+		
+		cart.setItems(itemsToAdd);
+		String result = ShoppingCartUtils.calculateTotal(cart);
+		
+		assertEquals("£1.20", result);			
+	}
+	
+	@Test 
+	void calculateTotal_OrangeOffer(){
+		ShoppingCart cart = new ShoppingCart();
+		List<String> itemsToAdd = Arrays.asList(ORANGE,ORANGE,ORANGE);
+		
+		cart.setItems(itemsToAdd);
+		String result = ShoppingCartUtils.calculateTotal(cart);
+		
+		assertEquals("£0.50", result);			
+	}
+	
+	@Test 
+	void calculateTotal_FourOrangeOffer(){
+		ShoppingCart cart = new ShoppingCart();
+		List<String> itemsToAdd = Arrays.asList(ORANGE,ORANGE,ORANGE,ORANGE);
+		
+		cart.setItems(itemsToAdd);
+		String result = ShoppingCartUtils.calculateTotal(cart);
+		
+		assertEquals("£0.75", result);			
+	}
+	
+	@Test 
+	void calculateTotal_MixOffer(){
+		ShoppingCart cart = new ShoppingCart();
+		List<String> itemsToAdd = Arrays.asList(ORANGE,APPLE,ORANGE,APPLE,ORANGE);
+		
+		cart.setItems(itemsToAdd);
+		String result = ShoppingCartUtils.calculateTotal(cart);
+		
+		assertEquals("£1.10", result);			
+	}
+	
+	@Test 
+	void calculateTotal_DoubleAppleOffer(){
+		ShoppingCart cart = new ShoppingCart();
+		List<String> itemsToAdd = Arrays.asList(ORANGE,APPLE,APPLE,APPLE,APPLE);
+		
+		cart.setItems(itemsToAdd);
+		String result = ShoppingCartUtils.calculateTotal(cart);
+		
+		assertEquals("£1.45", result);			
+	}
+	
+	@Test 
+	void calculateTotal_DoubleOrangeOffer(){
+		ShoppingCart cart = new ShoppingCart();
+		List<String> itemsToAdd = Arrays.asList(ORANGE,APPLE,ORANGE,ORANGE,ORANGE,ORANGE,ORANGE);
+		
+		cart.setItems(itemsToAdd);
+		String result = ShoppingCartUtils.calculateTotal(cart);
+		
+		assertEquals("£1.60", result);			
 	}
 
 }
